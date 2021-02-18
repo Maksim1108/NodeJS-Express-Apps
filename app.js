ebd93057d7c4c30148d9170c3c49e78b4cfe3e02
Maksim1108 <maksim.lyanko@bk.ru>
2021-02-18 17:22:57 +0600

    create form

--- app.js
+++ app.js
@@ -1,77 +1,68 @@
-const express = require('express')
-const morgan = require('morgan')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
+
+const dbURL =
+  "mongodb+srv://Maksim:Ebaloff1337228@cluster0.rmwa6.mongodb.net/NodeJS?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log("err", err));
 
 // express app
-const app = express()
-
-const dbURL = 'mongodb+srv://Maksim:Ebaloff1337228@cluster0.rmwa6.mongodb.net/NodeJS?retryWrites=true&w=majority'
-mongoose
-.connect(dbURL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then((res) => app.listen(3000))
-.catch((err) => console.log(err))
+const app = express();
 
 // listen for requests
+// app.listen(3000)
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
-app.use(express.static('public'))
+app.use(express.static("public"));
+app.use(express.urlencoded());
 
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
-const links = [{text:'Home', link:'/'},{text:'Articles', link:'/about'}]
+const links = [
+  { text: "Home", link: "/" },
+  { text: "Articles", link: "/about" },
+  { text: "Create", link: "/blogs/create" },
+];
 
-const blogs = [
-  {
-    img: '/img/computer.png',
-    title: 'long established',
-    description:
-      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that....',
-    time: 'May 20th 2020',
-  },
-  {
-    img: '/img/peoples.png',
-    title: 'long established',
-    description:
-      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that....',
-    time: 'May 20th 2020',
-  },
-  {
-    img: '/img/computer2.png',
-    title: 'long established',
-    description:
-      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that....',
-    time: 'May 20th 2020',
-  },
-]
+app.get("/", (request, responce) => {
+  Blog.find().then((result) =>
+    responce.render("index", { blogs: result, links })
+  );
+});
 
-app.get('/', (request, responce) => {
-  responce.render('index', { blogs, links }).status(200)
-})
-app.get('/add-blog', (request, responce) => {
+app.post("/blogs", (request, responce) => {
+  const blog = new Blog(request.body);
+  blog.save().then(() => {
+    responce.redirect("/blogs");
+  });
+});
+
+app.get("/add-blog", (req, res) => {
   const blog = new Blog({
-    title :'ахахахахахаха',
-    description: 'fsdff',
-    imgUrl:
-    '/img/computer2.png',
-    date: 'ПОШЕЛ ВОН'
-  })
-  blog
-  .save()
-  .then((result) => responce.send(result))
-})
-app.get('/blogs', (request, responce) => {
-  Blog.find().then((result) => {
-    responce.render('index', {blogs: result, links})
-  })
-})
-app.get('/about', (request, responce) => {
-  res.render('about', { blogs }).status(200)
-})
-app.get('/blogs/create', (req, res) => {
-  res.render('create').status(200)
-})
+    title: "long established",
+    description:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that....",
+    imgUrl: "/img/computer.png",
+    date: "May 20th 2020",
+  });
+  blog.save().then((result) => res.send(result));
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find().then((result) => res.render("index", { blogs: result, links }));
+});
+app.get("/about", (req, res) => {
+  Blog.find().then((result) => res.render("about", { blogs: result, links }));
+});
+app.get("/blogs/create", (req, res) => {
+  res.render("create").status(200);
+});
 app.use((req, res) => {
-  res.status(404).render('404')
-})
+  res.status(404).render("404");
+});
--- models/blog.js
+++ models/blog.js
@@ -1,24 +1,26 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema;
+
+const Schema = mongoose.Schema
 
 const blogSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    imgUrl: {
-        type: String,
-        required: true,
-    },
-    date: {
-        type: String,
-        required: true,
-    },
+  title: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  imgUrl: {
+    type: String,
+    require: true,
+  },
+  date: {
+    type: String,
+    require: true,
+  },
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
-module.exports = Blog;
+
+module.exports = Blog
